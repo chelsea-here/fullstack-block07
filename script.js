71c77cb706d45ee763ace39eb6bef27604c46597
Chelsea Cruz <125297600+chelsea-here@users.noreply.github.com>
2025-02-11 11:16:13 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// step one: declare variable for the string
+const alertString =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// log the variable to verify it works
+// console.log(alertString);
+
+// declare variables using mathematical operands for the code
+const vaultOne = 2 + 8;
+const vaultTwo = 4 * 10;
+const vaultThree = 40 - 1;
+// log the variables to verify they work
+// console.log(vaultOne);
+// console.log(vaultTwo);
+// console.log(vaultThree);
+
+// create an alert with provided string and variables
+alert(`${alertString} ${vaultOne} - ${vaultTwo} - ${vaultThree}`);
